42e79a4ca53dad2d21ebd978e815c86f9a60bdeb
benoit castillon <benoit.castillon@gmail.com>
2020-04-10 09:04:08 +0200

    model Realisation creation

---
+++ db/migrate/20200410065917_create_realisations.rb
@@ -0,0 +1,11 @@
+class CreateRealisations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :realisations do |t|
+      t.string :name
+      t.string :category
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
